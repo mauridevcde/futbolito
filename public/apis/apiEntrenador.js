a0c06e4900f6a0d8c68c25b621c81d6b22fc6294
ClaudioPanda <lic.claudio.landa@gmail.com>
2022-10-17 11:46:24 -0300

    cambio de ip AWS

--- public/apis/apiEntrenador.js
+++ public/apis/apiEntrenador.js
@@ -5,7 +5,7 @@ export function api_get_AllEntrenador() {
       redirect: "follow",
     };
 
-    fetch("http://ec2-18-216-42-40.us-east-2.compute.amazonaws.com:5050/futbolito/entrenadoresGetAll", requestOptions)
+    fetch("htpp://ec2-3-144-74-245.us-east-2.compute.amazonaws.com:5050/futbolito/entrenadoresGetAll", requestOptions)
       .then((response) => response.text())
       .then((result) => {
         //let data = JSON.parse(result)
@@ -37,7 +37,7 @@ export function api_post_Entrenador(data) {
       redirect: "follow",
     };
 
-    fetch("http://ec2-18-216-42-40.us-east-2.compute.amazonaws.com:5050/futbolito/entrenadorPost", requestOptions)
+    fetch("htpp://ec2-3-144-74-245.us-east-2.compute.amazonaws.com:5050/futbolito/entrenadorPost", requestOptions)
       .then((response) => response.text())
       .then((result) => {
         resolve(JSON.parse(result));
@@ -69,7 +69,7 @@ export function api_put_Entrenador(data) {
       redirect: 'follow'
     };
 
-    fetch("http://ec2-18-216-42-40.us-east-2.compute.amazonaws.com:5050/futbolito/entrenadorUpdate", requestOptions)
+    fetch("htpp://ec2-3-144-74-245.us-east-2.compute.amazonaws.com:5050/futbolito/entrenadorUpdate", requestOptions)
       .then(response => response.text())
       .then(result => { resolve(JSON.parse(result)); })
       .catch(error => console.log('error', error));
